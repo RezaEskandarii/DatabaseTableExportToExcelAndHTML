2e251b8debcff2d7c459f6fab43a591bb46f4b5e
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2019-06-02 22:52:42 +0430

    comments added to TableExport class

--- src/main/java/export/TableExport.java
+++ src/main/java/export/TableExport.java
@@ -3,18 +3,25 @@ package export;
 import connection.DatabaseConnection;
 
 import java.sql.Connection;
-
+/**
+* @super class for export classes
+*/
 public abstract class TableExport {
-
+    
+    //Separating data in Excel cells
     protected final static String TAB = ",";
+    
+    //write data in new line
     protected final static String NEXT_LINE = "\n";
 
     private DatabaseConnection databaseConnection = DatabaseConnection.getNewInstance();
 
     protected Connection connection = databaseConnection.getConnection();
 
+    //database exports file name
     protected String fileName;
-
+    
+    //query for data export
     protected String sqlQuery;
 
     public abstract void export();
